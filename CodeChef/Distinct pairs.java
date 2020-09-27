0cfa1637265daa24d2e4ef5ed8b8bbe851187aab
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-09-27 11:54:26 +0530

    Add files via upload

---
+++ CodeChef/Distinct pairs.java
@@ -0,0 +1,173 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+	        Reader sc=new Reader();
+	        int n = sc.nextInt();
+		int m = sc.nextInt();
+		long[] A = new long[n];
+		long[] B = new long[m];
+		for(int i=0;i<n;i++){
+	        A[i] = sc.nextLong();
+		}
+		for(int j=0;j<m;j++){
+	        B[j] = sc.nextLong();
+		}
+		long min = Long.MAX_VALUE;
+		long max = Long.MIN_VALUE;
+		int minIdx = -1;
+		int maxIdx = -1;
+		for (int i = 0; i < n; i++) {
+            
+            if(A[i] < min) {
+                min = A[i];
+                minIdx =i;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if(B[i] > max) {
+                max = B[i];
+                maxIdx =i;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+             System.out.println(minIdx+" "+i);
+        }
+        for (int i = 0; i < n; i++) {
+            if(i!=minIdx)
+              System.out.println(i+" "+maxIdx);
+        }
+	    }catch(Exception e){
+	        System.out.println(e);
+	    }
+	}
+	
+
+//////////////////////////This is Nothing///////////////////////////////////////////////
+	static class Reader 
+    { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+  
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+  
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            }  while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+  
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+  
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+  
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    }
+}
